b066249ef338bf4c6bdb951034787e5e5fedb93a
varun3702 <50037987+varun3702@users.noreply.github.com>
2019-04-27 16:04:58 +1000

    Update Moodlist.java

--- Moodlist.java
+++ Moodlist.java
@@ -1,22 +1,22 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Moodlist{
-  ArrayList<String> happy = new ArrayList<>(Arrays.asList(""));
-  ArrayList<String> sad = new ArrayList<>(Arrays.asList(""));
-  ArrayList<String> angry = new ArrayList<>(Arrays.asList(""));
-  ArrayList<String> love = new ArrayList<>(Arrays.asList(""));
-  ArrayList<String> wow = new ArrayList<>(Arrays.asList(""));
+  ArrayList<String> happy = new ArrayList<>(Arrays.asList("happy"));
+  ArrayList<String> sad = new ArrayList<>(Arrays.asList("sad"));
+  ArrayList<String> angry = new ArrayList<>(Arrays.asList("angry"));
+  ArrayList<String> love = new ArrayList<>(Arrays.asList("love"));
+  ArrayList<String> wow = new ArrayList<>(Arrays.asList("wow"));
 
 
-  public static void moodadd(Arraylist<String> mood, String mymood){
+  public static void moodadd(ArrayList<String> mood, String mymood){
     if(mood.contains(mymood)==false){
       mood.add(mymood);
       }
 
   }
 
-  public static void moodremove(Arraylist<String> mood, String mymood){
+  public static void moodremove(ArrayList<String> mood, String mymood){
     if(mood.contains(mymood)){
       mood.remove(mymood);
       }
