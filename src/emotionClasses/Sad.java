016aa8891d990c3d4a9352a81c31a007934b2ded
Edwin-Choi <edwinchoi1@hotmail.com>
2019-04-27 16:17:49 +1000

    reorg

--- src/emotionClasses/Sad.java
+++ src/emotionClasses/Sad.java
@@ -1,11 +1,35 @@
 import java.util.List;
 
-public class Sad extends Emotion
-{	
+public class Sad implements Emotions
+{
+	List<String> sad;
 	
-	public Sad(List<String> sad_list) 
+	public Sad() 
 	{
-		super(sad_list);
+		//sad = function call to get list version from the text file
+	}
+	
+	public void add(String key)
+	{
+		sad.add(key);
+	}
+	
+	public void remove(String key)
+	{
+		sad.remove(key);
+	}
+	
+	public Boolean duplicates(String key)
+	{
+		for (int i = 0; i < sad.size()-1; i++)
+		{
+			if (sad.get(i) == sad.get(i+1))
+			{
+				sad.remove(i);
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
