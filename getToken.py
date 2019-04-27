93d31d31e1b20e759d2ff913ee49775f32685b74
Eddie C <edwinchoi1@gmail.com>
2019-04-28 00:48:57 +1000

    to get the token

---
+++ getToken.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+
+gettoken = "curl -X \"POST\" -H \"Authorization: Basic MjVkYjBmNWY2M2UwNDQ2ZmExNmI4OWFiNjA0YmIxNWI6MzZmYTZhNjM1OGM0NGQxZTlmNTNlMzA4N2U0NzUwYTU=\" -d grant_type=client_credentials https://accounts.spotify.com/api/token"
+
+token = os.system(gettoken)
+
+print(token)
+	
